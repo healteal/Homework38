2ebc1af14a95321d822c159d5a82fb2e5b1cc62b
healteal <kirylshuppa@gmail.com>
2022-09-19 13:20:54 +0300

    Split solution

--- src/Task3/General.java
+++ src/Task3/General.java
@@ -3,12 +3,37 @@ package Task3;
 import java.io.*;
 
 public class General {
+    static int counter = 0;
+
     public static void main(String[] args) {
-        int counter = 0;
         System.out.println("Введите путь к файлу:");
         String path = reader();
         System.out.println("Введите искомое слово:");
         byte[] word = reader().getBytes();
+        solution(getTextFromFile(path), getWord(word));
+    }
+
+    public static String reader() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String returned;
+        try {
+            returned = reader.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return returned;
+    }
+
+    private static void solution(String[] text, String word) {
+        for (String s : text) {
+            if (s.equals(word)) {
+                counter++;
+            }
+        }
+        System.out.println(counter + " слова");
+    }
+
+    public static String[] getTextFromFile(String path) {
         File file = new File(path);
         FileInputStream fileInputStream;
         byte[] fileText = new byte[(int) file.length()];
@@ -19,31 +44,17 @@ public class General {
             throw new RuntimeException(e);
         }
         StringBuilder fileTextString = new StringBuilder();
-        StringBuilder wordStringBuilder = new StringBuilder();
         for (byte b : fileText) {
             fileTextString.append((char) b);
         }
-        String[] splitText = String.valueOf(fileTextString).split(" ");
+        return String.valueOf(fileTextString).split(" ");
+    }
+
+    public static String getWord(byte[] word) {
+        StringBuilder wordStringBuilder = new StringBuilder();
         for (byte b : word) {
             wordStringBuilder.append((char) b);
         }
-        String wordString = String.valueOf(wordStringBuilder);
-        for (String s : splitText) {
-            if (s.equals(wordString)) {
-                counter++;
-            }
-        }
-        System.out.println(counter + " слова");
-    }
-
-    private static String reader() {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String returned;
-        try {
-            returned = reader.readLine();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return returned;
+        return String.valueOf(wordStringBuilder);
     }
 }
